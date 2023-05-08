dd72054d732f674cfbdfd45586f98003ee8fa410
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-05-08 15:38:35 +0400

    Keep Hydrated

---
+++ DONE/Keep Hydrated!(DONE).js
@@ -0,0 +1,9 @@
+function litres(time) {
+  return Math.floor(time * 0.5)
+}
+
+console.log(litres(3))
+console.log(litres(2))
+console.log(litres(1.4))
+console.log(litres(6.7))
+console.log(litres(11.8))
