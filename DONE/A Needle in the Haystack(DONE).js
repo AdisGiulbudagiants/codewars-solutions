e7be58f7cbfb1a258f91d462d6bd8c88a6bb305e
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-05-11 00:17:09 +0400

    A Needle in the Haystack

---
+++ DONE/A Needle in the Haystack(DONE).js
@@ -0,0 +1,30 @@
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle")
+}
+
+console.log(
+  findNeedle([
+    "3",
+    "123124234",
+    undefined,
+    "needle",
+    "world",
+    "hay",
+    2,
+    "3",
+    true,
+    false,
+  ])
+)
+
+console.log(
+  findNeedle([
+    "283497238987234",
+    "a dog",
+    "a cat",
+    "some random junk",
+    "a piece of hay",
+    "needle",
+    "something somebody lost a while ago",
+  ])
+)
