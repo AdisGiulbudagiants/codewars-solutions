ecbc82160e094735dbc1d1d22d7a9c3dc4e6fa4e
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-06-03 11:57:04 +0400

    Removing Elements JS

---
+++ JS/DONE/Removing Elements(DONE).js
@@ -0,0 +1,9 @@
+function removeEveryOther(arr) {
+  const res = arr.filter((el, index) => index % 2 === 0)
+  return res
+}
+
+console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"])) //['Hello', 'Hello Again']
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) //[1, 3, 5, 7, 9]
+console.log(removeEveryOther([[1, 2]])) //[[1, 2]]
+console.log(removeEveryOther([["Goodbye"], { Great: "Job" }])) //[['Goodbye']]
