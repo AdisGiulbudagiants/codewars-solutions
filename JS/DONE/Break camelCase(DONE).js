231c02b7809e39b74103ef84941c47aea75cb5d9
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-06-07 19:39:33 +0400

    Break camelCase JS

---
+++ JS/DONE/Break camelCase(DONE).js
@@ -0,0 +1,7 @@
+function solution(string) {
+  return string.replace(/([A-Z])/g, " $1")
+}
+
+console.log(solution("camelCasing")) //"camel Casing"
+console.log(solution("camelCasingTest")) //'camel Casing Test'
+console.log(solution("identifier")) //"identifier"
