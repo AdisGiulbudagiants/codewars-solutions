a6caa5cbcf22cd30a7c571f6a3d05acba59f4fdb
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-06-23 12:01:53 +0400

    Small enough - Beginner JS

---
+++ JS/DONE/Small enough - Beginner(DONE).js
@@ -0,0 +1,9 @@
+function smallEnough(a, limit) {
+  return a.every((value) => value <= limit)
+}
+
+console.log(smallEnough([66, 101], 200)) //true
+console.log(smallEnough([2, 0, 0, 6, 0, 4, 7, 7, 0, 0, 0], 6)) //false
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)) //false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)) //true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)) //true
