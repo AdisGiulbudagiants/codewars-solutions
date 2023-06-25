9b371c4b69f0bb30dba1b77916fa3e5561e93df3
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-06-25 11:19:20 +0400

    Find the capitals JS

---
+++ JS/DONE/Find the capitals(DONE).js
@@ -0,0 +1,11 @@
+var capitals = function (word) {
+  let result = []
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == word[i].toUpperCase()) {
+      result.push(i)
+    }
+  }
+  return result
+}
+
+console.log(capitals("CodEWaRs")) //[0,3,4,6]
