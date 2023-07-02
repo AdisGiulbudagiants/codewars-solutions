7c8166f792c80a303a812fa574f07e059b829bd0
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-07-02 09:44:12 +0400

    Lario and Muigi Pipe Problem JS

---
+++ JS/DONE/Lario and Muigi Pipe Problem(DONE).js
@@ -0,0 +1,11 @@
+function pipeFix(numbers) {
+  let result = []
+  for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+    result.push(i)
+  }
+  return result
+}
+
+console.log(pipeFix([1, 2, 3, 5, 6, 8, 9])) //[1,2,3,4,5,6,7,8,9]
+console.log(pipeFix([1, 2, 3, 12])) //[1,2,3,4,5,6,7,8,9,10,11,12]
+console.log(pipeFix([6, 9])) //[6,7,8,9]
