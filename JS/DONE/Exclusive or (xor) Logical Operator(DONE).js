c5320aabdf363a48dcdffb9bcd7a63aed681e54b
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-07-09 10:58:11 +0400

    Exclusive or (xor) Logical operator JS

---
+++ JS/DONE/Exclusive or (xor) Logical Operator(DONE).js
@@ -0,0 +1,8 @@
+function xor(a, b) {
+  return a ^ b ? true : false
+}
+
+console.log(xor(false, false)) //false
+console.log(xor(true, false)) //true
+console.log(xor(false, true)) //true
+console.log(xor(true, true)) //false
