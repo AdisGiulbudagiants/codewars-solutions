1dacbd93f193c2e9ecb50e6c0f2a6aabdd34fe0d
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-07-11 14:15:47 +0400

    Check the exam

---
+++ JS/DONE/Check the exam(DONE).js
@@ -0,0 +1,18 @@
+function checkExam(array1, array2) {
+  let result = 0
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      result += 4
+    } else if (array2[i] === "") {
+      result += 0
+    } else {
+      result -= 1
+    }
+  }
+  return Math.max(0, result)
+}
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])) //6
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""])) //7
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"])) //16
+console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"])) //0
