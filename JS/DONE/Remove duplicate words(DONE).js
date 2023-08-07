8a3331db76be205adc581ddc965d30991d8f3077
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-08-07 23:36:12 +0400

    Remove duplicate words JS

---
+++ JS/DONE/Remove duplicate words(DONE).js
@@ -0,0 +1,9 @@
+function removeDuplicateWords(s) {
+  return [...new Set(s.split(" "))].join(" ")
+}
+
+console.log(
+  removeDuplicateWords(
+    "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+  )
+) //'alpha beta gamma delta'
