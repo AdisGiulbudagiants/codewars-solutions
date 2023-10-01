cc668d17eab40a522b5ab624fcf1cc1c6d867524
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-10-01 21:48:59 +0400

    Are the numbers in order JS

---
+++ JS/DONE/Are the numbers in order(DONE).js
@@ -0,0 +1,8 @@
+function inAscOrder(arr) {
+  return arr.every((el, index) => index === 0 || el >= arr[index - 1])
+}
+
+console.log(inAscOrder([1, 2, 4, 7, 19])) //true
+console.log(inAscOrder([1, 2, 3, 4, 5])) //true
+console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20])) //false
+console.log(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1])) //false
