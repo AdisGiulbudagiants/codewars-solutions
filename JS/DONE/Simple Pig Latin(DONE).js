da10e7677e519759afc9d346619ceb0ff0ba5c15
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-10-03 13:31:36 +0400

    Simple Pig Latin JS

---
+++ JS/DONE/Simple Pig Latin(DONE).js
@@ -0,0 +1,11 @@
+function pigIt(str) {
+  return str
+    .split(" ")
+    .map((el) => {
+      return /^[a-zA-Z]+$/.test(el) ? el.slice(1) + el[0] + "ay" : el
+    })
+    .join(" ")
+}
+
+console.log(pigIt("Pig latin is cool")) //igPay atinlay siay oolcay
+console.log(pigIt("Hello world !")) //elloHay orldway !
