f142f6f6a40218a5ac54f1e50c2b1132852cfcde
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-11-07 22:11:15 +0400

    Sum of odd numbers JS

---
+++ JS/DONE/Sum of odd numbers(DONE).js
@@ -0,0 +1,8 @@
+function rowSumOddNumbers(n) {
+  return n ** 3
+}
+
+console.log(rowSumOddNumbers(1)) //1
+console.log(rowSumOddNumbers(2)) //8
+console.log(rowSumOddNumbers(3)) //27
+console.log(rowSumOddNumbers(42)) //74088
