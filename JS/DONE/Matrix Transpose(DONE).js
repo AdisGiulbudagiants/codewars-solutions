25c0c1e13904a12917e8fba60dc3e626c65acf2b
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-03-21 15:41:28 +0400

    Matrix Transpose JS

---
+++ JS/DONE/Matrix Transpose(DONE).js
@@ -0,0 +1,11 @@
+function transpose(matrix) {
+  return matrix[0].map((_, index) => matrix.map((row) => row[index]))
+}
+
+console.log(transpose([[1]])) //[[1]]
+console.log(
+  transpose([
+    [1, 2, 3],
+    [4, 5, 6],
+  ])
+) //[[1,4],[2,5],[3,6]]
