382ba5716b326b568cef84713d69ac48c90e4aaf
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-04-17 11:05:00 +0700

    Building Strings From a Hash JS

---
+++ JS/DONE/Building Strings From a Hash(DONE).js
@@ -0,0 +1,11 @@
+function solution(pairs) {
+  return Object.entries(pairs)
+    .map(([key, value]) => `${key} = ${value}`)
+    .join(",")
+}
+
+console.log(solution({ a: 1, b: "2" })) //"a = 1,b = 2"
+console.log(solution({ a: "b", b: "a" })) //'a = b,b = a'
+console.log(solution({ 0: "a", b: 2 })) //'0 = a,b = 2'
+console.log(solution({ b: 1, c: 2, e: 3 })) //'b = 1,c = 2,e = 3'
+console.log(solution({})) //''
