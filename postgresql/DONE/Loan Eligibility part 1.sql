d535e8393fef6bbd02fb662ef68016743e36ae6c
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-09-22 12:00:34 +0700

    Loan Eligibility part 1 SQL

---
+++ postgresql/DONE/Loan Eligibility part 1.sql
@@ -0,0 +1,10 @@
+SELECT c.id AS customer_id,
+  CASE
+      WHEN c.age BETWEEN 18 AND 65
+            AND NOT EXISTS (SELECT 1 FROM loans l WHERE l.customer_id = c.id AND l.loan_status = 'unpaid')
+      THEN 'loan can be given'
+      ELSE 'loan cannot be given'
+  END AS loan_eligibility
+FROM customers c
+WHERE c.id BETWEEN 1 AND 10
+ORDER BY c.id DESC;
