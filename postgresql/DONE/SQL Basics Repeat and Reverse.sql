afc298c03f3b910c29a1e5f49978c45f253025db
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-10-03 20:53:31 +0700

    SQL Basics Repeat and Reverse SQL

---
+++ postgresql/DONE/SQL Basics Repeat and Reverse.sql
@@ -0,0 +1,5 @@
+SELECT REPEAT(name, 3) as name, REVERSE(characteristics) AS characteristics
+FROM monsters;
+
+--REPEAT повторяет значение столбца столько раз, сколько указано
+--REVERSE переворачивает слово задом наперед
